eb72de510bb2db2ec370ed0ded490e4b0c2acc6a
Nehemiah Henry <nehemiahelihenry@gmail.com>
2023-02-23 16:56:07 -0500

    Assignment Completed, Stretch Delivs. 1 + 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const form = document.getElementById("create-task-form")
+const taskInput = document.getElementById("new-task-description")
+const taskList = document.getElementById("tasks")
+const dropdown = document.getElementById("dropdown")
+// // const submitButton = document.querySelector(".submit")
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const li = document.createElement("li")
+  taskList.append(li)
+  li.innerText = taskInput.value
+  const dB = document.createElement("button")
+  li.append(dB)
+  dB.textContent = "x"
+
+  dB.addEventListener("click", () =>{
+    li.remove()
+  })
+  
+  if(dropdown.value === "hi") {
+      li.style.color = "red"
+    }
+  
+  if(dropdown.value === "med") {
+    li.style.color = "orange"
+  }
+
+  if(dropdown.value === "lo") {
+    li.style.color = "lime"
+  }
+  })
+
+// const form = document.getElementById("create-task-form")
+// const input = document.getElementById("new-task-description")
+// const ul = document.getElementById("tasks")
+
+// form.addEventListener("submit", (e)=>{
+// e.preventDefault()
+// const li = document.createElement("li")
+// ul.append(li)
+// li.innerText = input.value
+// const button = document.createElement("button")
+// li.append(button)
+// button.innerText = ":white_square_button:"
+// button.addEventListener("click", ()=>{
+// li.remove()
+// })
+// })
+
+
+
+
+
+
